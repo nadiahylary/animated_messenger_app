7bf7723da6f66778cdbd9322046f99a243ed5685
Nadia HYLARY <nhylary12@gmail.com>
2023-06-23 18:18:28 +0100

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,153 @@
 import 'package:flutter/material.dart';
 
+import 'animations.dart';
+import 'destinations.dart';
+import 'models/data.dart' as data;
+import 'models/models.dart';
+import 'widgets/emails_list.dart';
+import 'transitions/list_detail_transition.dart';
+import 'widgets/animated_floating_action_button.dart';
+import 'widgets/disappearing_bottom_nav_bar.dart';
+import 'widgets/disappearing_nav_rail.dart';
+import 'widgets/reply_list.dart';
+
 void main() {
   runApp(const MainApp());
 }
+final theme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(180, 250, 150, 150))
+
+);
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: Feed(currentUser: data.user_0),
+    );
+  }
+}
+
+class Feed extends StatefulWidget {
+  const Feed({
+    super.key,
+    required this.currentUser,
+  });
+
+  final User currentUser;
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
+  late final _colorScheme = Theme.of(context).colorScheme;
+  late final _backgroundColor = Color.alphaBlend(
+      _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
+  late final _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      reverseDuration: const Duration(milliseconds: 1250),
+      value: 0,
+      vsync: this
+  );
+  late final _railAnimation = RailAnimation(parent: _controller);
+  late final _railFabAnimation = RailFabAnimation(parent: _controller);
+  late final _barAnimation = BarAnimation(parent: _controller);
+  int selectedIndex = 0;
+
+  // bool wideScreen = false;
+  bool controllerInitialized = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final double width = MediaQuery.of(context).size.width;
+    //wideScreen = width > 600;
+    final AnimationStatus status = _controller.status;
+    if (width > 600) {
+      if (status != AnimationStatus.forward &&
+          status != AnimationStatus.completed) {
+        _controller.forward();
+      }
+    } else {
+      if (status != AnimationStatus.reverse &&
+          status != AnimationStatus.dismissed) {
+        _controller.reverse();
+      }
+    }
+    if (!controllerInitialized) {
+      controllerInitialized = true;
+      _controller.value = width > 600 ? 1 : 0;
+    }
+  }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, _) {
+        return Scaffold(
+          body: Row(
+            children: [
+              DisappearingNavigationRail(
+                railAnimation: _railAnimation,
+                railFabAnimation: _railFabAnimation,
+                selectedIndex: selectedIndex,
+                backgroundColor: _backgroundColor,
+                onDestinationSelected: (index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+              ),
+              Expanded(
+                child: Container(
+                  color: _backgroundColor,
+                  child: ListDetailTransition(
+                    animation: _railAnimation,
+                    one: EmailsList(
+                      selectedIndex: selectedIndex,
+                      onSelected: (index) {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      currentUser: widget.currentUser,
+                    ),
+                    two: const ReplyList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: AnimatedFloatingActionButton(
+            animation: _barAnimation,
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: DisappearingBottomNavigationBar(
+            barAnimation: _barAnimation,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
